ad7efc61cdd5105cd72f647e58c2fe34f245b179
Ignium12 <kevin.ruehl12@googlemail.com>
2021-01-25 11:59:50 +0100

    Initial commit

---
+++ src/main/java/com/kruehl/springsecurity/demo/config/DemoAppConfig.java
@@ -0,0 +1,13 @@
+package com.kruehl.springsecurity.demo.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+
+@Configuration
+@EnableWebMvc
+@ComponentScan
+public class DemoAppConfig {
+
+
+}
