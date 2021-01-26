618a8174f79a611c7fab06f4ee820493234a2214
Ignium12 <kevin.ruehl12@googlemail.com>
2021-01-26 14:22:09 +0100

    Added entities role and user, added a CRM user class, changed pom.xml dependencies, added passwordEncoder and authenticationProvider

--- src/main/java/com/kruehl/springsecurity/demo/config/DemoSecurityConfig.java
+++ src/main/java/com/kruehl/springsecurity/demo/config/DemoSecurityConfig.java
@@ -1,11 +1,14 @@
 package com.kruehl.springsecurity.demo.config;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 import javax.sql.DataSource;
 
@@ -47,5 +50,20 @@ public class DemoSecurityConfig extends WebSecurityConfigurerAdapter {
                 .permitAll()
                 .and()
                 .exceptionHandling().accessDeniedPage("/access-denied");
+    }
+
+    // bcrypt bean definition
+    @Bean
+    public BCryptPasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder();
+    }
+
+    // authentication provider bean definition
+    @Bean
+    public DaoAuthenticationProvider authenticationProvider(){
+        DaoAuthenticationProvider auth = new DaoAuthenticationProvider();
+        auth.setUserDetailsService(userService);
+        auth.setPasswordEncoder(passwordEncoder());
+        return auth;
     }
 }
---
+++ src/main/java/com/kruehl/springsecurity/demo/entity/role.java
@@ -0,0 +1,39 @@
+package com.kruehl.springsecurity.demo.entity;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "role")
+public class role {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+
+    @Column(name = "name")
+    private String userName;
+
+    public role() {
+    }
+
+    public role(String userName) {
+        this.userName = userName;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+}
